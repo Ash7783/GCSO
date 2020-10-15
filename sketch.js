0ec5d8af011c18de28403794a941c0c916ca7693
Ash7783 <72360735+Ash7783@users.noreply.github.com>
2020-10-15 16:39:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,16 +3,15 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-  speed=random(1,3);
+  speed=random(55,90);
   weight=random(400,1500);
   car.velocityX=speed;
 }
 
 function draw() {
-  background(0,0,0);  
+  background(255,255,255);  
   car=createSprite(50,200,50,50);
   wall=createSprite(1500,200,60,height/2);
-  wall.shapeColor=(black);
 
   if(wall.x-car.x<(car.width+wall.width)/2 && car.x-wall.x<(car.width+wall.width)/2)
   {
